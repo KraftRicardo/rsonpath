eb5f88315424864feb462febb5a1a72c0c785394
Ricardo Kraft <kraftricardo97@gmail.com>
2025-04-27 06:29:08 +0200

    Changed repetition count for eval_final.rs

--- crates/rsonpath-lib/src/lookup_table/final_results/eval_final.rs
+++ crates/rsonpath-lib/src/lookup_table/final_results/eval_final.rs
@@ -40,10 +40,10 @@ pub fn evaluate(data_dir_path: &str, base_path: &str) {
     println!("serde_json_path");
 
     // GB_1
-    eval_all(&data_dir_path, &base_path, QUERY_BESTBUY);
-    eval_all(&data_dir_path, &base_path, QUERY_CROSSREF1);
-    eval_all(&data_dir_path, &base_path, QUERY_CROSSREF2);
-    eval_all(&data_dir_path, &base_path, QUERY_CROSSREF4);
+    // eval_all(&data_dir_path, &base_path, QUERY_BESTBUY);
+    // eval_all(&data_dir_path, &base_path, QUERY_CROSSREF1);
+    // eval_all(&data_dir_path, &base_path, QUERY_CROSSREF2);
+    // eval_all(&data_dir_path, &base_path, QUERY_CROSSREF4);
     eval_all(&data_dir_path, &base_path, QUERY_GOOGLE);
     eval_all(&data_dir_path, &base_path, QUERY_NSPL);
     eval_all(&data_dir_path, &base_path, QUERY_TWITTER);
