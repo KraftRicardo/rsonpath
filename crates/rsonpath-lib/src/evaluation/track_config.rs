92541052ec11365ba5e2111bfa1521c977135175
Ricardo Kraft <kraftricardo97@gmail.com>
2025-07-20 22:49:01 +0200

    New parameters 3

--- crates/rsonpath-lib/src/evaluation/track_config.rs
+++ crates/rsonpath-lib/src/evaluation/track_config.rs
@@ -1,5 +1,5 @@
 // CONFIG
 // Tells whether the skip jump distance shall be tracked during run time. Will slow down the algorithm a lot!
-pub const TRACK_SKIPPING_ON: bool = true;
+pub const TRACK_SKIPPING_ON: bool = false;
 // Number of warm-up repetitions and real repetitions used for the performance tests
 pub const QUERY_REPETITIONS: u64 = 30;
